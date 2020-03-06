8197c35005b4a73115003ad435266fb6f53bee8c
Ankitha <vankithareddy07@gmail.com>
2020-03-06 17:49:32 +0530

    your commit message

---
+++ src/test/java/com/vnrvjiet/ankitha/SimpleInterest/SI/AppTest.java
@@ -0,0 +1,20 @@
+package com.vnrvjiet.ankitha.SimpleInterest.SI;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
